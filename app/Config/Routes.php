84de4d463e629fe983839e36cb685e3d665604b4
Maeva-co <maevartmbts@gmail.com>
2026-04-29 11:56:51 +0300

    template

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+// juste l'implementation de la template de base, a modifier en fonction de nos pages
 $routes->get('/', 'Home::index');
+$routes->get('/dashboard', 'Home::dashboard');
+$routes->get('/list', 'Home::list');
+$routes->get('/form', 'Home::form');
 
 
 $routes->get('/students', 'StudentController::index');
